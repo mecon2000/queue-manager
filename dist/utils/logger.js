ba8b312ffbee22510f6935aed34763ccc8954fa9
Ronnie Ganot <mecon2000@gmail.com>
2025-07-10 14:52:05 +0300

    added gitignore, removed dist and node_module from repo

--- dist/utils/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-// Simple logger utility. Replace with a more robust logger (e.g., winston) for production.
-exports.logger = {
-    info: (msg) => console.log(`[INFO] ${msg}`),
-    error: (msg) => console.error(`[ERROR] ${msg}`),
-};
